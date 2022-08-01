09ae25a0551eb9f4535f4d25729f5a4d1c61d80e
Evelyn Arevalo <evelynarevalow@gmail.com>
2022-07-31 19:42:15 -0700

    first commit

---
+++ middleware/auth-user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const auth = require('basic-auth');
+
+// Middleware to authenticate the request using Basic Authentication.
+exports.authenticateUser = async (req, res, next) => {
+    // TODO
+    next();
+}
